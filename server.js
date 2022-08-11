371445495ed68022a66c7abd987bb03648fee288
Mary Frye <deimospho@gmail.com>
2022-08-11 16:13:07 -0400

    ADDED mongoose and cors to server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+// DEPENDENCIES
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
-require('dotenv').config()
 
+//MIDDLEWARE
+require('dotenv').config()
+const PORT = process.env.PORT
+app.use(express.json())
+app.use(cors())
+
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo on: ', process.env.MONGO_URI)
+})
+
+// ROOT INDEX
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+// BOOKS 
+const booksController = require('./controllers/books_controller')
+app.use('/books', booksController)
 
-app.listen(process.env.PORT)
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
